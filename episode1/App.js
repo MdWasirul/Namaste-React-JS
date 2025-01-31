61a7e121a3bd23163e766ec15974701920756759
The SwiftCoder <techbymdwasirul2024@gmail.com>
2025-01-31 12:14:48 +0530

    Ep-01 and -02 Igniting Our app

--- episode1/App.js
+++ episode1/App.js
@@ -23,13 +23,22 @@
 //     React.createElement("h1", {}, "Hi I'm H1 Tag!")
 //   )
 // );
-// console.log(parent);
+// console.log(parent);s
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+// import { createRoot } from 'react-dom/client'; //suport react 19 
 
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement(
     "div",
     { id: "child1" },
-    React.createElement("h1", {}, "Hi I'm H1 Tag!")
+    React.createElement("h1", {}, "This is Namaste React!🚀")
+  ),
+  React.createElement(
+    "div",
+    { id: "child1" },
+    React.createElement("h1", {}, " By Akshay Saini!")
   ),
   React.createElement(
     "div",
@@ -40,8 +49,9 @@ const parent = React.createElement("div", { id: "parent" }, [
 console.log(parent);
 
 /*JSX is used to simplified the Above Code to write html  */
+// const root = createRoot(document.getElementById('root'));
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root =ReactDOM.createRoot(document.getElementById('root'));
 root.render(parent);
 
 // root.render(heading); //Render method change object to heading tag h1
